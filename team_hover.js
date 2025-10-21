475cb869c8994512866b724d48f2d0fe4f14e5af
Candela <thecorpsebutcher@gmail.com>
2025-10-21 18:34:23 +0200

    el equipo

---
+++ team_hover.js
@@ -0,0 +1,24 @@
+const areas = document.querySelectorAll('area');
+const info = document.getElementById('info');
+
+areas.forEach(area=> {
+    area.addEventListener('mouseover', () => {
+        const zona = area.dataset.zona; //leer data-zona
+
+
+        switch (zona){
+            case 'zonaEdu':
+                info.innerHTML = 
+                '<div class="miembro_info"><h3> EL EDU</h3> <h4> ARTISTA 3D y ARTISTA TÉCNICO</h4><p> Pues dibuja y modela muy bien</p></div>';
+
+                break;
+            default: 
+                info.textContent = " NADA ";
+        }
+    });
+
+    //Cuando el raton no está en la zona:
+    area.addEventListener('mouseout', () => {
+        info.textContent = ' ';
+    });
+});
